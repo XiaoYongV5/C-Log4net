2164e265ed54674d939ae82583832f5f6e52e965
kab <kab>
2017-07-20 10:51:05 +0800

    C# Log4net日志记录

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Emrys.FlashLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            FlashLogger.Debug("Debug");
+            FlashLogger.Debug("Debug", new Exception("testexception"));
+            FlashLogger.Info("Info");
+            FlashLogger.Fatal("Fatal");
+            FlashLogger.Error("Error");
+            FlashLogger.Warn("Warn", new Exception("testexception"));
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
